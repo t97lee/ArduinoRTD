b309996655cb01f9b84037a4ac278f0fef340c2e
Thomas Lee <51377707+thomaslee01@users.noreply.github.com>
2021-05-03 12:38:44 -0400

    Renamed from Backend.py to Arduino_Code.py

--- Arduino_Code.py
+++ Arduino_Code.py
@@ -7,19 +7,19 @@ it = util.Iterator(board)
 it.start()
 vol = board.get_pin("a:0:i")
 
-#variables for calculating the temperature
+#Temperature Calculations
 voltage_source = 5
-R_known = 1000 #Value of the known resistor 
-alpha = 0.00385 #temperature coefficient 
-temp_ref = -5 #temperature reference
-R_ref = 1000 #resistance of RTD (part no. F2222-1000-A-100)
+R_known = 1000
+alpha = 0.00385 #Temperature coefficient 
+temp_ref = -5 #Reference Temperature
+R_ref = 1000 #Resistance of RTD (part no. F2222-1000-A-100)
                                                                             
 pp = ProgressPlot()
 
 while True:
     voltages = vol.read() * 5 #scales voltage to 5V
     
-    R_rtd = (R_known) * (voltage_source - voltages)/voltages #eq. 1-8 to find R of RTD
+    R_rtd = (R_known) * (voltage_source - voltages)/voltages #eq. 1-8 to find resistance of RTD
     
     temps = temp_ref + (R_rtd / R_ref - 1) / alpha #eq. 3-3 to solve for T (temperature)
     
